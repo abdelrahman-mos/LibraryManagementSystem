4a54e6b423cdef024e1db966087d1ec7ab5f1f1b
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2024-04-10 01:02:48 +0200

    Adding Member class

--- LibraryManagementSystem/Member.py
+++ LibraryManagementSystem/Member.py
@@ -1,7 +1,12 @@
 import uuid
+from LibraryManagementSystem.Book import Book
+from datetime import datetime
 
 
 class Member:
     def __init__(self, name: str):
         self.name = name
         self.id = uuid.uuid4()
+        self.borrowedBooks: list[tuple[Book, datetime]] = []
+        self.reservedBooks: list[Book] = []
+        self._canBorrow = True
