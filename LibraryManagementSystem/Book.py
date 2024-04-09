a9d6629981d0caf743e76bf4458e6469cfca89c3
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2024-04-10 00:40:42 +0200

    Adding returnBook and reserveBook

--- LibraryManagementSystem/Book.py
+++ LibraryManagementSystem/Book.py
@@ -1,6 +1,7 @@
 import uuid
 from enum import Enum
 from LibraryManagementSystem.Member import Member
+import datetime
 
 
 class BookCategory(Enum):
@@ -10,14 +11,18 @@ class BookCategory(Enum):
 
 
 class Book:
-    def __init__(self, name: str, author: str, category: BookCategory, num: int):
+    def __init__(self, name: str, author: str, category: BookCategory, num: int = 1):
+
+        # Public attributes
         self.id = uuid.uuid4()
         self.name = name
         self.author = author
         self.category = category
+        # Private attributes
         self._num = num
         self._numAvailable = num
         self._borrowedBy: list[Member] = []
+        self._reservedBy: list[tuple[Member, datetime.datetime]] = []
 
     def borrowBook(self, borrowBy: Member):
         if self._numAvailable > 0:
@@ -26,7 +31,16 @@ class Book:
             return True
         return False
 
-    def __str__(self):
-        tmp = f"book name: {self.name}\nauthor: {self.author}\ncategory: {self.category}\nID: {self.id}\n"
-        return tmp
+    def reserveBook(self, reserveBy: Member):
+        date = datetime.datetime.now()
+        print(date)
+        self._reservedBy.append((reserveBy, date))
 
+    def returnBook(self, returnedBy: Member):
+        if returnedBy in self._borrowedBy:
+            self._borrowedBy.remove(returnedBy)
+            self._numAvailable += 1
+
+    # def __str__(self):
+    #     tmp = f"book name: {self.name}\nauthor: {self.author}\ncategory: {self.category}\nID: {self.id}\n"
+    #     return tmp
--- LibraryManagementSystem/Library.py
+++ LibraryManagementSystem/Library.py
@@ -1,6 +1,7 @@
 import os
 from LibraryManagementSystem.Book import Book
 from LibraryManagementSystem.Book import BookCategory
+from LibraryManagementSystem.Member import Member
 
 
 def cls():
@@ -9,10 +10,10 @@ def cls():
 
 class Library:
     def __init__(self):
-        self.books = []
+        self.books: list[Book] = []
 
-    def add_book(self, bookName: str, author: str, category: BookCategory):
-        book: Book = Book(name=bookName, author=author, category=category)
+    def add_book(self, bookName: str, author: str, category: BookCategory, num: int = 1):
+        book: Book = Book(name=bookName, author=author, category=category, num=num)
         print(book)
         self.books.append(book)
 
@@ -20,4 +21,10 @@ class Library:
     def run():
         lib: Library = Library()
         lib.add_book('idek', "who?", BookCategory.SCIENCE)
-
+        mem: Member = Member('Ahmed')
+        mem2: Member = Member('Mohamed')
+        x = lib.books[0].borrowBook(mem)
+        x = lib.books[0].borrowBook(mem2)
+        x = lib.books[0].reserveBook(mem2)
+        lib.books[0].returnBook(mem)
+        print("x")
